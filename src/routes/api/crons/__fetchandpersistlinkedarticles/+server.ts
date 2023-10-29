0fb4754bf8c4b589309d1dac987b678cab6b3822
TakueTodoApp <mombejose@gmail.com>
2023-10-29 03:58:35 +0200

    msg

--- src/routes/api/crons/__fetchandpersistlinkedarticles/+server.ts
+++ src/routes/api/crons/__fetchandpersistlinkedarticles/+server.ts
@@ -1,1 +1,15 @@
+import type { RequestHandler } from "./$types";
+
 // __fetchandpersistlinkedarticles
+const handler: RequestHandler = async({ur})=>{
+    const articleSources = new Map<string, string>();
+
+    async function getSourceID(source:{url:string, name:string}):Promise<string>{
+        let sourceID:string = articleSources.get(source.url) ?? "";
+        if(sourceID) return sourceID;
+
+        return (
+            await Services
+        )
+    }
+}
